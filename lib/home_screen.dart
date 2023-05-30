95d7b00d2fa9eaa9b587f5b15a0573c6d8ca6ff4
David Kpidiba <kpidibadavid1@gmail.com>
2023-05-30 14:30:08 +0000

    init calculator bloc

---
+++ lib/home_screen.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: const Text("CALCULATRICE"),
+        centerTitle: true,
+      ),
+      body: Center(),
+    );
+  }
+}
